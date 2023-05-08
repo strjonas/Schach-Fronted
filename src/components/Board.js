1361ab50c90718d49dbe023cf8f1f22e58e165a1
strjonas <48125614+strjonas@users.noreply.github.com>
2023-05-08 16:06:17 +0200

    better structure, move validation halfway done

--- src/components/Board.js
+++ src/components/Board.js
@@ -4,8 +4,19 @@ import { initialData } from '../data';
 import '../styles/App.css';
 
 
+
 export default function App() {
-  const [data, setData] = useState(initialData);
+    const [board, setBoard] = useState(initialData);
+    const [turn, setTurn] = useState('white')
+    const [rochade, setRochade] = useState([true, true, true, true]) // [whiteLeft, whiteRight, blackLeft, blackRight
+    const [enPassant, setEnPassant] = useState('')
+
+    const [promotion, setPromotion] = useState(null)
+    const [check, setCheck] = useState(null)
+    const [checkMate, setCheckMate] = useState(null)
+    const [staleMate, setStaleMate] = useState(null)
+    const [history, setHistory] = useState([])
+
 
   function isLowerCase(str)
     {
@@ -23,7 +34,7 @@ export default function App() {
     return array
     }
 
-    const arrayToData = (array) => {
+    const arrayToData = (array, data) => {
         let newData = {
             ...data,
             lists: {
@@ -38,83 +49,189 @@ export default function App() {
         }
         return newData
     }
+
+    // implementiert elias noch
+    const evaluateFen = (fenstring) => {}
     
-
-
-  const onDragEnd = (result) => {
-    const { destination, source } = result;
-
-    // Basic checks
-    if (!destination) return;
-    
-
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    ) return;
-
-    let sourcePiece = data.lists[source.droppableId];
-    let destinationPiece = data.lists[destination.droppableId];
-
-    // check if piece that is moved is actually a piece
-    if (sourcePiece.length === 0) return
-
-
-
-    // TODO zugvalidierung
-
-    // field is occupied
-    
-
-    // check if same or different color
-    if (isLowerCase(sourcePiece.slice(0, 1)) === isLowerCase(destinationPiece.slice(0, 1)))  {
-        // same color
-        if (destinationPiece.length > 0) return
+    function onReceiveFenFromServer(fenstring){
+        let obj = evaluateFen(fenstring)
+        setBoard(arrayToData(obj.board, board))
+        setTurn(obj.turn)
+        setRochade(obj.rochade)
+        setEnPassant(obj.enPassant)
     }
-    else{
-        // different color
-        // piece will be captured
+
+    const checkBasics = (source, destination, sourcePiece) => {
+        // Basic checks
+        if (!destination) return false;
+        
+        // check if piece that is moved is actually a piece
+        if (sourcePiece.length === 0) return false;
+
+        return !(destination.droppableId === source.droppableId &&
+        destination.index === source.index);
     }
-    
+
+    const pawnMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        // check if pawn is moved two fields
+        if (Math.abs(source.index - destination.index) === 16) {
+            // check if pawn is moved two fields but is not on starting position
+            if (source.index > 15 && source.index < 48) return false;
+            // check if pawn is moved two fields but is blocked by another piece
+            if (destinationPiece.length !== 0) return false;
+            // check if pawn is moved two fields but is blocked by another piece
+            if (board.lists[destination.droppableId.slice(0, 1) + (destination.index + 8).toString()].length !== 0) return false;
+        }
+
+        // check if pawn is moved one field
+        if (Math.abs(source.index - destination.index) === 8) {
+            // check if pawn is moved one field but is blocked by another piece
+            if (destinationPiece.length !== 0) return false;
+        }
+
+        // check if pawn is moved diagonally
+        if (Math.abs(source.index - destination.index) === 7 || Math.abs(source.index - destination.index) === 9) {
+            // false if field is empty and its not en passant
+            if (destinationPiece.length === 0 && enPassant !== destination.droppableId) return false;
+        }
+
+    }
+
+    const rookMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        // check if rook is moved diagonally
+        if (Math.abs(source.index - destination.index) % 8 !== 0 && Math.abs(source.index - destination.index) > 7) return false;
+        // check if rook is moved horizontally
+        if (Math.abs(source.index - destination.index) % 8 === 0 && Math.abs(source.index - destination.index) > 7) {
+            // check if rook is blocked by another piece
+            if (source.index < destination.index) {
+                for (let i = source.index + 8; i < destination.index; i += 8) {
+                    if (board.lists[destination.droppableId.slice(0, 1) + i.toString()].length !== 0) return false;
+                }
+            } else {
+
+                for (let i = source.index - 8; i > destination.index; i -= 8) {
+                    if (board.lists[destination.droppableId.slice(0, 1) + i.toString()].length !== 0) return false;
+                }
+            }
+        }
+
+        // check if rook is moved vertically
+        if (Math.abs(source.index - destination.index) % 8 !== 0 && Math.abs(source.index - destination.index) < 8) {
+            // check if rook is blocked by another piece
+            if (source.index < destination.index) {
+                for (let i = source.index + 1; i < destination.index; i++) {
+                    if (board.lists[destination.droppableId.slice(0, 1) + i.toString()].length !== 0) return false;
+                }
+            } else {
+                for (let i = source.index - 1; i > destination.index; i--) {
+                    if (board.lists[destination.droppableId.slice(0, 1) + i.toString()].length !== 0) return false;
+                }
+            }
+        }
+    }
+
+    const knightMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        // either x is 2 and y is 1 or x is 1 and y is 2
+        return true;
+    }
+
+    const bishopMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        return true;
+
+    }
+    const queenMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        return true;
+
+    }
+    const kingMoveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        return true;
+
+    }
 
 
 
+    const moveIsLegal = (source, destination, sourcePiece, destinationPiece) => {
+        let chessPiece = source.droppableId.slice(0, 1);
 
-    // TODO
+        // check if it was the players turn
+        if (isLowerCase(chessPiece) && turn === 'white') return false;
+        if (!isLowerCase(chessPiece) && turn === 'black') return false;
 
+        // check if field was occupied by own piece
+        if (destinationPiece.length !== 0) {
+            if (isLowerCase(sourcePiece) === isLowerCase(destinationPiece)) return false;
+        }
 
+        switch (chessPiece.toUpperCase()) {
+            case 'P':
+                return pawnMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            case 'R':
+                return rookMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            case 'N':
+                return knightMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            case 'B':
+                return bishopMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            case 'Q':
+                return queenMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            case 'K':
+                return kingMoveIsLegal(source, destination, sourcePiece, destinationPiece)
+            default:
+                return false
+        }
 
-    
+    }
 
-      
-    // hier wurde vorher geschaut ob sourceList === destinationList, ich weiss net warum, falls was nicht klappt, mal checken
-
-    
-    // Daten Setzen
-    const newData = {
-        ...data,
+    const setNewData = (source, destination, sourcePiece) => {
+        const newData = {
+        ...board,
         lists: {
-            ...data.lists,
+            ...board.lists,
             [source.droppableId]: '',
             [destination.droppableId]: sourcePiece,
         },
     };
-    setData(newData);
-    
+    setBoard(newData);
+    }
+
+    const sendMoveToServer = (move) => {
+
+    }
+
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+
+    let sourcePiece = board.lists[source.droppableId];
+    let destinationPiece = board.lists[destination.droppableId];
+
+        
+    // Basic checks
+    if (!checkBasics(source, destination, sourcePiece)) return;
+
+    // check if move is legal
+    if (!moveIsLegal(source, destination)) return;
+   
+
+    // set new data
+    setNewData(source, destination, sourcePiece);
+
+    // send move to server in chess notation i.e. e2e4
+    let move = source.droppableId + destination.droppableId	
+    sendMoveToServer(move);
+        
   }
   
   return (
     <DragDropContext onDragEnd={onDragEnd}>
         <div className="column">
-            {data.listOrder.map((liste, index) => {
+            {board.listOrder.map((liste, index) => {
                 return (
 
                     <div className="row" key={index}> 
                         {liste.map((listId, index) => {
                             
-                            const list = data.lists[listId];
+                            const list = board.lists[listId];
 
-                            const task = data.tasks[list]
+                            const task = board.tasks[list]
 
                             return (
                                 <Droppable droppableId={listId} key={listId}>
