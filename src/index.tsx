4e6a2bf6f1685529e6d65193ec146e31e4f78e09
strjonas <48125614+strjonas@users.noreply.github.com>
2023-05-22 10:50:34 +0200

    added typescript support

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.js';
+// @ts-ignore
+import App from './App.tsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
     <App />
 );
